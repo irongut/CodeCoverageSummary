94f2b3c7112f3a2020e43fe913c664e97b7dcccd
irongut <murray.dave@outlook.com>
2021-04-14 23:27:39 +0100

    improved help text

--- src/CodeCoverageSummary/CommandLineOptions.cs
+++ src/CodeCoverageSummary/CommandLineOptions.cs
@@ -7,13 +7,13 @@ namespace CodeCoverageSummary
         [Value(index: 0, Required = true, HelpText = "Code coverage file to analyse.")]
         public string Filename { get; set; }
 
-        [Option(shortName: 'f', longName: "format", Required = false, HelpText = "Output Format - markdown or text (default).", Default = "text")]
-        public string Format { get; set; }
-
-        [Option(shortName: 'b', longName: "badge", Required = false, HelpText = "Include a badge in the output - true / false (default).", Default = false)]
+        [Option(shortName: 'b', longName: "badge", Required = false, HelpText = "Include a badge in the output - true / false.", Default = false)]
         public bool Badge { get; set; }
 
-        [Option(shortName: 'o', longName: "output", Required = false, HelpText = "Output Type - console (default), file or both.", Default = "console")]
+        [Option(shortName: 'f', longName: "format", Required = false, HelpText = "Output Format - markdown or text.", Default = "text")]
+        public string Format { get; set; }
+
+        [Option(shortName: 'o', longName: "output", Required = false, HelpText = "Output Type - console, file or both.", Default = "console")]
         public string Output { get; set; }
     }
 }
