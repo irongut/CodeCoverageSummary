7c070aea386905d3c67473352a3d649d4e782222
irongut <murray.dave@outlook.com>
2021-10-19 00:03:22 +0100

    fixed issue with bool parameters

--- src/CodeCoverageSummary/CommandLineOptions.cs
+++ src/CodeCoverageSummary/CommandLineOptions.cs
@@ -1,4 +1,5 @@
 ﻿using CommandLine;
+using System;
 
 namespace CodeCoverageSummary
 {
@@ -7,14 +8,18 @@ namespace CodeCoverageSummary
         [Value(index: 0, Required = true, HelpText = "Code coverage file to analyse.")]
         public string Filename { get; set; }
 
-        [Option(longName: "badge", Required = false, HelpText = "Include a badge reporting the Line Rate coverage in the output using shields.io - true or false.", Default = false)]
-        public bool Badge { get; set; }
+        [Option(longName: "badge", Required = false, HelpText = "Include a badge reporting the Line Rate coverage in the output using shields.io - true or false.", Default = "false")]
+        public string BadgeString { get; set; }
+
+        public bool Badge => BadgeString.Equals("true", StringComparison.OrdinalIgnoreCase);
 
         [Option(longName: "format", Required = false, HelpText = "Output Format - markdown or text.", Default = "text")]
         public string Format { get; set; }
 
-        [Option(longName: "indicators", Required = false, HelpText = "Include package health indicators in the output - true or false.", Default = true)]
-        public bool Indicators { get; set; }
+        [Option(longName: "indicators", Required = false, HelpText = "Include package health indicators in the output - true or false.", Default = "true")]
+        public string IndicatorsString { get; set; }
+
+        public bool Indicators => IndicatorsString.Equals("true", StringComparison.OrdinalIgnoreCase);
 
         [Option(longName: "output", Required = false, HelpText = "Output Type - console, file or both.", Default = "console")]
         public string Output { get; set; }
